bbe334ab90830a1a6a5926355181a8c9c6e0b2b5
Clay Carpenter <claycarpenter@gmail.com>
2012-04-29 18:08:46 -0400

    Updated implementation of ManagedFixturesTestCase to use fixtures that are explicitly registered via an internal method call, rather than relying on the fixtures being members of a Fixture subclass.

--- coggrinder/core/test.py
+++ coggrinder/core/test.py
@@ -12,27 +12,32 @@ class ManagedFixturesTestCase(unittest.TestCase):
     up the declared fixtures for each unit test.
     '''
     def __init__(self, methodName='runTest'):
-        unittest.TestCase.__init__(self, methodName=methodName)       
-        
-    def _register_fixtures(self, *fixtures):
-        for fixture in fixtures:
-            self.fxtr.append(fixture)
-            
-    def setUp(self):
+        unittest.TestCase.__init__(self, methodName=methodName)
+
         # A registry of all of the fixtures common to the tests owned by this
         # TestCase.
-        self.fxtr = ManagedFixturesTestCase.FixtureRegistry()
-    
+        self._fixtures_registry = None
+
+    def _register_fixtures(self, reset_fixtures=True, *fixtures):
+        if reset_fixtures:
+            # Reset fixtures registry.
+            self._fixtures_registry = list()
+        
+        for fixture in fixtures:
+            self._fixtures_registry.append(fixture)
+
     def tearDown(self):
         # Clean up any class-level stubbing. This does not override stubbing
         # on individual mock instances, but that is remedied as long as the
         # mock instances are included in the fixtures that will be cleaned up
         # next.
         mockito.unstub()
-        
+
         # Clean up any declared fixtures.
-        del self.fxtr
-        
+        while self._fixtures_registry:
+            fixture = self._fixtures_registry.pop(0)
+            del fixture
+
     class FixtureRegistry(object):
         """Simple container class to hold arbitrary fixture references."""
 #------------------------------------------------------------------------------ 
