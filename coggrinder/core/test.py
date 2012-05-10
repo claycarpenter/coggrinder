757d59c331d064a256e832eea04e1a210747f1e5
Clay Carpenter <claycarpenter@gmail.com>
2012-05-10 07:38:18 -0400

    coggrinder.core.test: - Minor code formatting changes.

--- coggrinder/core/test.py
+++ coggrinder/core/test.py
@@ -7,22 +7,22 @@ Created on Apr 29, 2012
 import mockito
 
 class ManagedFixturesTestSupport(object):
-    '''Simple test case utility that managed the fixtures by establishing and 
+    '''Simple test case utility that managed the fixtures by establishing and
     cleaning up the declared fixtures for each unit test.
     '''
     def _register_fixtures(self, reset_fixtures=False, *fixtures):
         # TODO: This hasattr test doesn't smell very Pythonic...
-        if not hasattr(self,"_fixtures_registry") or reset_fixtures:
+        if not hasattr(self, "_fixtures_registry") or reset_fixtures:
             # Reset fixtures registry.
             self._fixtures_registry = list()
-        
+
         for fixture in fixtures:
             self._fixtures_registry.append(fixture)
 
     def tearDown(self):
         """Clears out any Mockito stubs and deletes any registered test
         fixtures."""
-        
+
         # Clean up any class-level stubbing. This does not override stubbing
         # on individual mock instances, but that is remedied as long as the
         # mock instances are included in the fixtures that will be cleaned up
