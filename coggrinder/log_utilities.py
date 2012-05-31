d469ff5ff472c18e05bd23a512acce9deb9a3804
Clay Carpenter <claycarpenter@gmail.com>
2012-05-30 21:57:43 -0400

    Updates the entryExit log decorator to support more output options.
    Updates the entryExit log decorator to support two new output options:
    the ability to record the arguments the wrapped method was called with
    and the result of the wrapped method call.
    
    Reworks the simple test process to ensure that the decorator handles
    instance attributes appropriately, and to exercise the new output
    options.

--- coggrinder/log_utilities.py
+++ coggrinder/log_utilities.py
@@ -5,6 +5,7 @@ Created on May 28, 2012
 '''
 
 import logging
+import pprint
 
 TRACE_ENTER = "ENTER"
 TRACE_EXIT = "EXIT"
@@ -21,42 +22,75 @@ class entryExit(object):
     """
     _TRACE_MESSAGE_FORMAT = "TRACE {action} - module: {module_name}; method: {method_name}"
     
-    def __init__(self, logging_context=None, level=logging.DEBUG):
+    def __init__(self, show_args=False, show_result=False, logging_context=None, level=logging.DEBUG):        
         if logging_context is None:
             logging_context = {}
             
         self.logging_context = logging_context
-        self.level = level
+        self.level = level    
+        self.show_args = show_args
+        self.show_result = show_result    
         
     def __call__(self, wrapped_function):                
-        def entryExitLogger(*args, **kwargs):
+        def wrapper(*args, **kwargs):     
             log_message = entryExit._TRACE_MESSAGE_FORMAT.format(
-                action=TRACE_ENTER, method_name=wrapped_function.__name__, 
+                action=TRACE_ENTER, method_name=wrapped_function.__name__,
                 module_name=wrapped_function.__module__)
             logging.log(self.level, log_message, extra=self.logging_context)
             
+            if self.show_args:
+                logging.log(self.level, "Arguments:", extra=self.logging_context)
+                logging.log(self.level, pprint.pformat(args), extra=self.logging_context)
+                logging.log(self.level, "Keyword Arguments:", extra=self.logging_context)
+                logging.log(self.level, pprint.pformat(kwargs), extra=self.logging_context)
+                
             result = wrapped_function(*args, **kwargs)
+            
+            if self.show_result:
+                logging.log(self.level, "Result:", extra=self.logging_context)
+                logging.log(self.level, pprint.pformat(result), extra=self.logging_context)
                         
             log_message = entryExit._TRACE_MESSAGE_FORMAT.format(
-                action=TRACE_EXIT, method_name=wrapped_function.__name__, 
+                action=TRACE_EXIT, method_name=wrapped_function.__name__,
                 module_name=wrapped_function.__module__)
             logging.log(self.level, log_message, extra=self.logging_context)
             
             return result
-    
-        return entryExitLogger
+        
+        return wrapper
 #------------------------------------------------------------------------------
 
 """
 TODO: This example and the main method that runs a quick test should be 
 replaced by a proper unittest test case.
 """
-class QuickExample(object):
-    @entryExit()
-    def traced_method(self, message="Nothing"):
-        print "Message: {0}".format(message)      
+class Person(object):
+    def __init__(self, name):
+        self.name = name
         
-        return "Result of the event execution."
+    @entryExit(show_args=True, show_result=True)
+    def wave(self, other_person):
+        logging.debug("{self_name} waves at {other_name}".format(self_name=self.name,other_name=other_person.name))      
+        
+        return True
+        
+    @entryExit()
+    def say_hello(self):
+        logging.debug("Hello, my name is {0}".format(self.name))      
+        
+        return True
+    
+    @staticmethod
+    @entryExit()
+    def square(x):
+        return x * x
+    
+    def __repr__(self):
+        # Simple representation to make the log records look nicer.
+        return "<Person object, name: {name}>".format(name=self.name)
+    
+    def __str__(self):
+        return self.__repr__()
 #------------------------------------------------------------------------------
 
 if __name__ == '__main__':    
@@ -64,8 +98,16 @@ if __name__ == '__main__':
     logging.debug("Logging is configured.")
     
     logging.debug("Testing trace log system.")
-    example = QuickExample()
-    result = example.traced_method()
-    result = example.traced_method("Using logger")
-    logging.debug("Trace result: {0}".format(result))
-    logging.debug("Done... hopefully.")
+    
+    ann = Person("Ann")
+    jon = Person("Jon")
+    
+    ann.say_hello()
+    jon.say_hello()
+    
+    ann.wave(jon)
+    jon.wave(ann)
+    
+    logging.debug("Square: {0}".format(str(Person.square(8))))
+    
+    logging.debug("Done.")
